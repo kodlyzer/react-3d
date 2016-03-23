495d0b047db01194583615e6a3b2a38b28e8e8eb
Karthik Rana <karthikr@qburst.com>
2016-03-23 20:52:13 +0530

    initializes  with base set ups and test component

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import RotatingCube from './RotatingCube';
+
+class App extends Component {
+
+	constructor() {
+
+        super();
+
+
+    }
+
+
+    componentDidMount() {
+
+        
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    render() {
+        return (<RotatingCube />);
+    }
+}
+export default App;
---
+++ src/components/RotatingCube.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class RotatingCube extends Component {
+
+    constructor() {
+
+        super();
+
+    }
+
+	render() {
+
+	    return (<h1>Test</h1>);
+
+	}
+}
+
+export default RotatingCube;
