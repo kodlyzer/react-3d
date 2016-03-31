023defdd033169e134d7bf5a126b8d7e3e266430
Hari Krishnan <hari@qburst.com>
2016-03-31 14:46:11 +0530

    new changes

--- src/components/RotatingCube.js
+++ src/components/RotatingCube.js
@@ -9,34 +9,55 @@ class RotatingCube extends React.Component {
     componentDidMount() {
         var scene = new THREE.Scene();
         var renderer = new THREE.WebGLRenderer();
-        var camera = new THREE.PerspectiveCamera(
-            75,
-            window.innerWidth / window.innerHeight,
-            1,
-            10000);
+        var camera = new THREE.PerspectiveCamera(45,window.innerWidth / window.innerHeight,1,10000);
 
-        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.setSize(200, 200);
+        renderer.setClearColor( 0xffffff, 1);
+
         this.refs.rotatingCube.appendChild(renderer.domElement);
-
-        var geometry = new THREE.BoxGeometry(
-            500,
-            500,
-            500,
-            10,
-            10,
-            10);
-        var material = new THREE.MeshBasicMaterial({
-            color: 0xfffff,
-            wireframe: true
-        });
+        
+        var geometry = new THREE.BoxGeometry(500,500,500);
+        var texture1 = THREE.ImageUtils.loadTexture('./src/assets/images/1.jpg');
+        texture1.anisotropy = renderer.getMaxAnisotropy();
+        var texture2 = THREE.ImageUtils.loadTexture('./src/assets/images/2.png');
+        texture2.anisotropy = renderer.getMaxAnisotropy();
+        var texture3 = THREE.ImageUtils.loadTexture('./src/assets/images/3.jpg');
+        texture3.anisotropy = renderer.getMaxAnisotropy();
+        var texture4 = THREE.ImageUtils.loadTexture('./src/assets/images/4.jpg');
+        texture4.anisotropy = renderer.getMaxAnisotropy();
+        var texture5 = THREE.ImageUtils.loadTexture('./src/assets/images/5.jpg');
+        texture5.anisotropy = renderer.getMaxAnisotropy();
+        var texture6 = THREE.ImageUtils.loadTexture('./src/assets/images/6.jpg');
+        texture6.anisotropy = renderer.getMaxAnisotropy();
+        
+        var material = new THREE.MeshFaceMaterial([
+            new THREE.MeshBasicMaterial({
+                 map: texture1
+            }),
+            new THREE.MeshBasicMaterial({
+                 map: texture2
+            }),
+            new THREE.MeshBasicMaterial({
+                map: texture3
+            }),
+            new THREE.MeshBasicMaterial({
+                 map: texture4
+            }),
+            new THREE.MeshBasicMaterial({
+                 map: texture5
+            }),
+            new THREE.MeshBasicMaterial({
+                 map: texture6
+            })
+        ]);
         var cube = new THREE.Mesh(geometry, material);
         scene.add(cube);
-        camera.position.z = 1000;
+        camera.position.z = 1200;
 
         function render() {
             requestAnimationFrame(render);
             cube.rotation.x += 0.01;
-            cube.rotation.y += 0.01;
+            cube.rotation.y += 0.03;
             renderer.render(scene, camera);
         };
         render();
